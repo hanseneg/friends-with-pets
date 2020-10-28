63624f5beee7a42d91d3a271123be10fa059b1c0
Ethan Hansen <hanseneg@hotmail.com>
2020-10-28 12:16:11 -0600

    commit5

--- src/Friend.js
+++ src/Friend.js
@@ -1,5 +1,18 @@
-import React from "react"
-import friends from "./FriendList"
+import React from 'react'
+import Pet from './Pet'
 
+function Friend(props) {
+    const petInfo = props.pets.map(pets => <Pet name={pets.name} breed={pets.breed} />)
+    return (
+        <div>
+            <h1>Name: {props.name}</h1>
+            <p>Age: {props.age}</p>
+            <h2>Pets</h2>
+            <hr></hr>
+            <p>{petInfo}</p>
+        </div>
+    )
+}
 
-// map through friendsInfo-friends
+export default Friend
+
--- src/Pet.js
+++ src/Pet.js
@@ -1,1 +1,12 @@
-import React from "react"
+import React from 'react'
+
+function Pet(props) {
+    return (
+        <div>
+            <p>Name: {props.name}</p>
+            <p>Breed: {props.breed}</p>
+        </div>
+    )
+}
+
+export default Pet
